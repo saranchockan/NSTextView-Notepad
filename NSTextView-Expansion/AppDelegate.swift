9ce3507669b599ec221dd0ee04063df85a0a6404
Saran Chockan <saranjitharunesh@gmail.com>
2018-02-28 17:14:50 -0600

    Initial Commit

---
+++ NSTextView-Expansion/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSTextView-Expansion
+//
+//  Created by Saranjitharunesh Chockan on 2/28/18.
+//  Copyright © 2018 Saranjitharunesh Chockan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NSTextView-Expansion/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSTextView-Expansion
+//
+//  Created by Saranjitharunesh Chockan on 2/28/18.
+//  Copyright © 2018 Saranjitharunesh Chockan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
